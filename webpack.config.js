55d7b62a36731f7831b65fdbd06fdd90f76c0a2d
Igor Deryabin <igor@rodweb.ru>
2017-09-03 02:58:56 +0200

    Add project configuration

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src',
+
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader', options: { presets: ['env'] } },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, 'dist'),
+    watchContentBase: true,
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify(require('./package.json').version),
+    }),
+  ],
+};
