f95dfc661fc4f3054f148109ad1299293efbfd6c
Igor Deryabin <igor@rodweb.ru>
2017-09-08 15:21:18 +0200

    Page layout fixes (0.0.2)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import './app.css';
 import './field.css';
 
-const $version = document.querySelector('.js-version');
-$version.innerText = VERSION;
+document.addEventListener('DOMContentLoaded', onInit);
+
+function onInit() {
+  setVersion();
+}
+
+function setVersion() {
+  document.querySelector('.app-version').innerText = `v${VERSION}`;
+}
